03a170f7fd3590e5390a271e997d94be84fad35e
Yuancheng Wu <johnwu0202@gmail.com>
2019-04-04 17:02:32 -0700

    add router and a few components

---
+++ src/main/MainMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function MainMenu() {
+  return (
+    <div>
+      <div>Main Menu</div>
+      <ul>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import NavLink from './NavLink';
+
+export default function Navbar() {
+  return <NavLink />;
+}
